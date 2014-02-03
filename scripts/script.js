ed925624f18b0daa1e9a9e15199ffdcd3c35aa69
LynnSunwoo <lynnsunwoo@gmail.com>
2014-02-03 02:47:10 -0500

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+window.onload = hello;
+
+function hello(){
+
+	$('.hovers').hide();
+
+	$('.col').hover(function(){
+		$(this).children('.hovers').show();
+	});
+
+	$('.col').mouseleave(function(){
+		$('.hovers').hide();
+	});
+
+
+
+};
